2e385f96509f28075b870c267dd7a0b772bf9e21
Mees Buschman <mezeman1@gmail.com>
2024-09-08 13:28:12 +0200

    Revert "chore: env"
    This reverts commit fea08611e939300f3adcd57cc89e011daac4bffb.

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getAnalytics } from 'firebase/analytics'
 // ... other firebase imports
 
 export const firebaseApp = firebase.initializeApp({
-  apiKey: import.meta.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VUE_APP_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyC-iXDOWE7d6YFbTPklOXgkK-ca7ePlI9w',
+  authDomain: 'idleantfarm.firebaseapp.com',
+  projectId: 'idleantfarm',
+  storageBucket: 'idleantfarm.appspot.com',
+  messagingSenderId: '862631118579',
+  appId: '1:862631118579:web:1b40be2d081fc2d0ed5986',
+  measurementId: 'G-LWCZP322K6',
 })
 
 // used for the firestore refs
@@ -23,3 +23,4 @@ const initFirebase = async () => {
 
 
 export { db, analytics, initFirebase }
+
