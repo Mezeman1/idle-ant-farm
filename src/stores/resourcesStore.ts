02bc729c9a0d2ab48ab57762ff886fe98d3504d1
Mees Buschman <mezeman1@gmail.com>
2024-09-19 19:15:24 +0200

    fix: not equal

--- src/stores/resourcesStore.ts
+++ src/stores/resourcesStore.ts
@@ -181,8 +181,8 @@ export const useResourcesStore = defineStore('resources', {
     createMaxAnts(fromPrestige = false) {
       if (
         fromPrestige
-        && this.resources.larvae <= useSettingsStore().autoThresholds.autoAntCreationLarvae / 100 * this.storage.maxLarvae
-        && this.resources.seeds <= useSettingsStore().autoThresholds.autoAntCreationSeeds / 100 * this.storage.maxSeeds
+        && this.resources.larvae < useSettingsStore().autoThresholds.autoAntCreationLarvae / 100 * this.storage.maxLarvae
+        && this.resources.seeds < useSettingsStore().autoThresholds.autoAntCreationSeeds / 100 * this.storage.maxSeeds
       ) {
         return
       }
@@ -212,7 +212,7 @@ export const useResourcesStore = defineStore('resources', {
       this.resources.antHousing += 1
     },
     createMaxAntHousing(fromPrestige = false) {
-      if (fromPrestige && this.resources.seeds <= this.storage.maxSeeds * useSettingsStore().autoThresholds.autoCreateHousing / 100) {
+      if (fromPrestige && this.resources.seeds < this.storage.maxSeeds * useSettingsStore().autoThresholds.autoCreateHousing / 100) {
         return
       }
 
@@ -251,8 +251,8 @@ export const useResourcesStore = defineStore('resources', {
     createEliteMaxAnts(fromPrestige = false) {
       if (
         fromPrestige
-        && this.resources.larvae <= useSettingsStore().autoThresholds.autoEliteAntsCreationLarvae / 100 * this.storage.maxLarvae
-        && this.resources.seeds <= useSettingsStore().autoThresholds.autoEliteAntsCreationSeeds / 100 * this.storage.maxSeeds
+        && this.resources.larvae < useSettingsStore().autoThresholds.autoEliteAntsCreationLarvae / 100 * this.storage.maxLarvae
+        && this.resources.seeds < useSettingsStore().autoThresholds.autoEliteAntsCreationSeeds / 100 * this.storage.maxSeeds
       ) {
         return
       }
@@ -289,8 +289,8 @@ export const useResourcesStore = defineStore('resources', {
     buyMaxQueens(fromPrestige = false) {
       if (
         fromPrestige
-        && this.resources.ants <= useSettingsStore().autoThresholds.autoQueenCreationAnts / 100 * this.storage.maxAnts
-        && this.resources.seeds <= useSettingsStore().autoThresholds.autoQueenCreationSeeds / 100 * this.storage.maxSeeds
+        && this.resources.ants < useSettingsStore().autoThresholds.autoQueenCreationAnts / 100 * this.storage.maxAnts
+        && this.resources.seeds < useSettingsStore().autoThresholds.autoQueenCreationSeeds / 100 * this.storage.maxSeeds
       ) {
         return
       }
@@ -321,7 +321,7 @@ export const useResourcesStore = defineStore('resources', {
       this.resources.seeds += seedsToAdd
     },
     upgradeSeedStorage(fromPrestige = false) {
-      if (fromPrestige && this.resources.seeds <= useSettingsStore().autoThresholds.autoSeedStorageUpgrade / 100 * this.storage.maxSeeds) {
+      if (fromPrestige && this.resources.seeds < useSettingsStore().autoThresholds.autoSeedStorageUpgrade / 100 * this.storage.maxSeeds) {
         return
       }
 
@@ -390,7 +390,7 @@ export const useResourcesStore = defineStore('resources', {
     },
     // Function to upgrade larvae storage
     upgradeLarvaeStorage(fromPrestige = false) {
-      if (fromPrestige && this.resources.larvae <= useSettingsStore().autoThresholds.autoLarvaeStorageUpgrade / 100 * this.storage.maxLarvae) {
+      if (fromPrestige && this.resources.larvae < useSettingsStore().autoThresholds.autoLarvaeStorageUpgrade / 100 * this.storage.maxLarvae) {
         return
       }
 
