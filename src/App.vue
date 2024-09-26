b16af2bf8443e621b9d4a6555ee3758d46e2f75a
Mees Buschman <mezeman1@gmail.com>
2024-09-26 22:06:11 +0200

    only show on mobile

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { onMounted } from 'vue'
 import {useToast} from 'vue-toast-notification'
 
 onMounted(() => {
-  if (document.fullscreenElement === null) {
+  if (document.fullscreenElement === null && isMobile) {
     const toast = useToast()
     toast.info('You are not in fullscreen mode. You need to be fullscreen to see the entire UI.', {
       position: 'top',
@@ -15,4 +15,6 @@ onMounted(() => {
     document.documentElement.requestFullscreen()
   }
 })
+
+const isMobile = window.innerWidth < 640
 </script>
