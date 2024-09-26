6e7f2d67d0219b5b0f97ae6ba2eb9a3deb5271f9
Mees Buschman <mezeman1@gmail.com>
2024-09-26 22:03:33 +0200

    feat: fullscreen

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,14 @@
 </template>
 
 <script lang="ts" setup>
+import { onMounted } from 'vue'
+import {useToast} from 'vue-toast-notification'
+
+onMounted(() => {
+  if (document.fullscreenElement === null) {
+    const toast = useToast()
+    toast.info('You are not in fullscreen mode. You need to be fullscreen to see the entire UI.')
+    document.documentElement.requestFullscreen()
+  }
+})
 </script>
