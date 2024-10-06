88c2815b5cbcb1a00f3808260fdd15de74f88a89
Mees <mees@sevenlab.nl>
2024-10-06 12:58:00 +0200

    chore: remove title of passives

--- src/views/PassivePage.vue
+++ src/views/PassivePage.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="p-6 bg-white bg-opacity-80 rounded-xl shadow-lg relative flex flex-col h-full overflow-y-auto">
-    <h1 class="text-3xl font-extrabold mb-6 text-gray-800">Passive Items Inventory</h1>
     <!-- Passive Items Grid -->
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       <div
