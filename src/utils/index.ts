84464d070acd53fdf0eeb22acf2d140d93da7b27
Sebastian Švec <74354294+svecs132@users.noreply.github.com>
2024-10-12 22:41:45 +0200

    extracted time formatting into a function

---
+++ src/utils/index.ts
@@ -0,0 +1,14 @@
+const formatTime = (milliseconds: number, hideZeros = false): string => {
+  const days = Math.floor(milliseconds / (1000 * 60 * 60 * 24))
+  const hours = Math.floor((milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds = Math.floor((milliseconds % (1000 * 60)) / 1000)
+
+  if (hideZeros) {
+    return `${days > 0 ? `${days}d ` : ''}${hours > 0 ? `${hours}h ` : ''}${minutes > 0 ? `${minutes}m ` : ''}${seconds}s`
+  } else {
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`
+  }
+}
+
+export { formatTime }
