57bfa9b22e6757423d8ad37ac185ed93d8ed9452
Mees <mees@sevenlab.nl>
2024-11-02 13:20:50 +0100

    added drops

--- src/types/items/sets/abyssalSet.ts
+++ src/types/items/sets/abyssalSet.ts
@@ -27,4 +27,104 @@ export const abyssalSet = [
     },
     evolves: true,
   },
+  {
+    id: 'abyssal-chestplate',
+    name: 'Abyssal Chestplate',
+    description: 'Chestplate that enhances storage by 1.5% per level.',
+    type: 'equipment',
+    equipmentType: 'armor',
+    slotType: 'body',
+    set: 'Abyssal Set',
+    rarity: 'epic',
+    level: 1,
+    maxLevel: () => {
+      return useEvolveStore().getMaxItemLevel('Abyssal Set')
+    },
+    multiplier: 0.015,
+    effect: ({equipmentStore}, item: Item) => {
+      const bonusMultiplier = 1 + (item.multiplier * item.level)
+      equipmentStore.storageModifier *= bonusMultiplier
+      return true
+    },
+    onRemove: ({equipmentStore}, item: Item) => {
+      const bonusMultiplier = 1 + (item.multiplier * item.level)
+      equipmentStore.storageModifier /= bonusMultiplier
+    },
+    evolves: true,
+  },
+  {
+    id: 'abyssal-boots',
+    name: 'Abyssal Boots',
+    description: 'Boots that enhance storage by 1.5% per level.',
+    type: 'equipment',
+    equipmentType: 'armor',
+    slotType: 'legs',
+    set: 'Abyssal Set',
+    rarity: 'epic',
+    level: 1,
+    maxLevel: () => {
+      return useEvolveStore().getMaxItemLevel('Abyssal Set')
+    },
+    multiplier: 0.015,
+    effect: ({equipmentStore}, item: Item) => {
+      const bonusMultiplier = 1 + (item.multiplier * item.level)
+      equipmentStore.storageModifier *= bonusMultiplier
+      return true
+    },
+    onRemove: ({equipmentStore}, item: Item) => {
+      const bonusMultiplier = 1 + (item.multiplier * item.level)
+      equipmentStore.storageModifier /= bonusMultiplier
+    },
+    evolves: true,
+  },
+  {
+    id: 'abyssal-sword',
+    name: 'Abyssal Sword',
+    description: 'Sword that enhances army attack by 1.5% per level.',
+    type: 'equipment',
+    equipmentType: 'weapon',
+    slotType: 'weapon',
+    set: 'Abyssal Set',
+    rarity: 'epic',
+    level: 1,
+    maxLevel: () => {
+      return useEvolveStore().getMaxItemLevel('Abyssal Set')
+    },
+    multiplier: 0.015,
+    effect: ({adventureStore}, item: Item) => {
+      const bonusMultiplier = 1 + (item.multiplier * item.level)
+      adventureStore.armyAttackModifier *= bonusMultiplier
+      return true
+    },
+    onRemove: ({adventureStore}, item: Item) => {
+      const bonusMultiplier = 1 + (item.multiplier * item.level)
+      adventureStore.armyAttackModifier /= bonusMultiplier
+    },
+    evolves: true,
+  },
+  {
+    id: 'abyssal-shield',
+    name: 'Abyssal Shield',
+    description: 'Shield that enhances army defense by 1.5% per level.',
+    type: 'equipment',
+    equipmentType: 'shield',
+    slotType: 'shield',
+    set: 'Abyssal Set',
+    rarity: 'epic',
+    level: 1,
+    maxLevel: () => {
+      return useEvolveStore().getMaxItemLevel('Abyssal Set')
+    },
+    multiplier: 0.015,
+    effect: ({equipmentStore}, item: Item) => {
+      const bonusMultiplier = 1 + (item.multiplier * item.level)
+      equipmentStore.defenseModifier *= bonusMultiplier
+      return true
+    },
+    onRemove: ({equipmentStore}, item: Item) => {
+      const bonusMultiplier = 1 + (item.multiplier * item.level)
+      equipmentStore.defenseModifier /= bonusMultiplier
+    },
+    evolves: true,
+  },
 ]
